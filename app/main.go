bd2e9313e4554c5b2002004e2f250076aeffdabc
VaudKK <vaudkeith@gmail.com>
2025-11-03 00:29:38 +0300

    feat: REPL

--- app/main.go
+++ app/main.go
@@ -10,14 +10,18 @@ var _ = fmt.Fprint
 var _ = os.Stdout
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	if err != nil {
-		fmt.Fprint(os.Stderr,"Error reading input")
-		return
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Fprint(os.Stderr,"Error reading input")
+			return
+		}
+
+		fmt.Fprint(os.Stdout,command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Fprint(os.Stdout,command[:len(command)-1] + ": command not found")
+	
 }
