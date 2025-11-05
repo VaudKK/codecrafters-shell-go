f6ae4200d306b2aeb4d3c7fc048e83b54093553b
VaudKK <vaudkeith@gmail.com>
2025-11-05 22:32:17 +0300

    debug

--- app/echo.go
+++ app/echo.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func echoCommand(args []string) {
 	out := ""
 
 	for _, arg := range args {
-		unQuoted, err := strconv.Unquote(arg)
-
-		if err != nil {
-			out += arg
-		}else{
-			out += unQuoted
-		}
+		// get the string within quotes
+		out += parseString(arg, '\'')
 	}
 
 	fmt.Fprintf(os.Stdout, "%s\n", out)
+}
+
+func parseString(value string, quoteChar byte) string {
+	result := ""
+
+	if len(value) <= 2 {
+		return result
+	}
+
+	// if the first and last char are the quote char remove the quotes
+	if value[0] == quoteChar && value[len(value)-1] == quoteChar {
+		result = value[1 : len(value)-1]
+	}
+
+	return result
 }
