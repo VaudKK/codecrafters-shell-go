9fc74dceb13233785cf987861652d9a69fbc02f1
VaudKK <vaudkeith@gmail.com>
2025-11-19 23:40:22 +0300

    debug: double quotes

---
+++ app/helpers.go
@@ -0,0 +1,37 @@
+package main
+
+
+func splitWithQuotes(s string) []string {
+	var result []string
+	var current string
+	inSingleQuote := false
+	inDoubleQuote := false
+
+	for i := 0; i < len(s); i++ {
+
+		if s[i] == '"' {
+			inDoubleQuote = !inDoubleQuote
+		}
+
+		if s[i] == '\'' {
+			if inDoubleQuote {
+				result = append(result, string(s[i]))
+			}else{
+				inSingleQuote = !inSingleQuote
+			}
+		} else if s[i] == ' ' && !inSingleQuote && !inDoubleQuote {
+			if current != "" {
+				result = append(result, current)
+				current = ""
+			}
+		} else {
+			current += string(s[i])
+		}
+	}
+
+	if current != "" {
+		result = append(result, current)
+	}
+	
+	return result
+}
